5d93a121c80cc44ad39fc2c53343d34c5fc28a68
think2011 <452125301@qq.com>
2014-10-21 16:55:16 +0800

    完成 lessons4

--- lesson4/app.js
+++ lesson4/app.js
@@ -1,28 +1,70 @@
 var cheerio    = require('cheerio');
 var superAgent = require('superagent');
+var eventproxy = require('eventproxy');
+var express    = require('express');
+var url        = require('url');
+
+var targetUrl = 'http://www.qiushibaike.com/';
 
 var app = express();
 
-app.get('/', function (req, res, next) {
-	superAgent.get('https://cnodejs.org/')
-	.end(function (err, sres) {
-		if (err) { return next(err) }
-
-		var $ 	  = cheerio.load(sres.text);
-		var items = [];
-
-		$('#topic_list .topic_title').each(function (idx, element) {
-			var $element = $(element);
-
-			items.push({
-				title: $element.attr('title'),
-				href : $element.attr('href')
-			});
-		});
-
-		res.send(items);
-	});
+app.get('/', function (req, res) {
+    getData(function (data) {
+        res.send(data);
+    });
 });
+
+/**
+ * 获取糗事百科的第一页链接&内容&评论
+ * @param {function} cb
+ * @return {array}
+ */
+function getData(cb) {
+    superAgent.get(targetUrl).end(function (err, res) {
+        if(err) {return console.error(err)}
+
+        var urls = [];
+        var $ = cheerio.load(res.text);
+
+        // 获取所有链接
+        $('.qiushi_comments').each(function (index, element) {
+            var $element = $(element);
+            var href = url.resolve(targetUrl, $element.attr('href'));
+
+            urls.push(href);
+        });
+
+        // 获取链接内的第一条评论
+        var ep = new eventproxy();
+
+        // 批量请求链接
+        urls.forEach(function (url) {
+            superAgent.get(url).end(function (err, res) {
+                if(err) {return console.error(err)}
+
+                // 提交结果到ep
+                ep.emit('finishOfComment', [url, res.text]);
+            });
+        });
+
+        // 处理接收的结果
+        ep.after('finishOfComment', urls.length, function (results) {
+            results = results.map(function (pair) {
+                var $ = cheerio.load(pair[1]);
+
+                return {
+                    url: pair[0],
+                    content:  $('.content').text().trim(),
+                    comment1: $('.comment-block .replay .body').eq(0).text().trim()
+                }
+            });
+
+            // callback
+            cb(results);
+        });
+    });
+}
+
 
 app.listen(3000, function () {
 	console.log('app is running at port 3000');
